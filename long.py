df7b486d1be62886fd51213808a471e7521f04a8
arif <OrangeBlu20R>
2021-06-13 01:08:45 +1000

    hello

--- long.py
+++ long.py
@@ -1,30 +1,29 @@
 from Bio import SeqIO
 import io
 
-def find_overlaps(reads_list, superstring=''):
+def get_superstring(reads_list, superstring=''):
     if len(reads_list) == 0:
         return superstring
 
     elif len(superstring) == 0:
         superstring = reads_list.pop(0)
-        return find_overlaps(reads_list, superstring)
+        return get_superstring(reads_list, superstring)
 
     else:
-
         for current_read_index in range(len(reads_list)):
             current_read = reads_list[current_read_index]
             current_read_length = len(current_read)
 
-            for current_read_character_index in range(current_read_length // 2):
-                overlap_length = current_read_length - current_read_character_index
+            for trial in range(current_read_length // 2):
+                overlap_length = current_read_length - trial
 
-                if superstring.startswith(current_read[current_read_character_index:]):
+                if superstring.startswith(current_read[trial:]):
                     reads_list.pop(current_read_index)
-                    return find_overlaps(reads_list, current_read[:current_read_character_index] + superstring)
+                    return get_superstring(reads_list, current_read[:trial] + superstring)
 
                 if superstring.endswith(current_read[:overlap_length]):
                     reads_list.pop(current_read_index)
-                    return find_overlaps(reads_list, superstring + current_read[overlap_length:])
+                    return get_superstring(reads_list, superstring + current_read[overlap_length:])
 
 
 if __name__ == "__main__":
@@ -36,12 +35,13 @@ CCTGCCGGAA
 >Rosalind_58
 AGACCTGCCG
 >Rosalind_59
-GCCGGAATAC"""
+GCCGGAATAC
+"""
     handle = io.StringIO(small_dataset)
 
     reads = []
     for record in SeqIO.parse(handle, 'fasta'):
         reads.append(str(record.seq))
     handle.close()
-    ov = find_overlaps(reads)
+    ov = get_superstring(reads)
     print(ov)
